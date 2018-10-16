8873485199a8240ba1f5f9f2132b0a62dab274d1
Seth Feder <sethfeder2017@u.northwestern.edu>
2018-10-16 12:51:25 -0500

    added actual typing indicator dots

--- client/components/TypingIndicator.js
+++ client/components/TypingIndicator.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types'
 import { StyleSheet, View, Text } from 'react-native';
 import { VirgilCrypto } from 'virgil-crypto';
 import virgil from '../utils/virgilUtil';
+import AnimatedEllipsis from 'react-native-animated-ellipsis';
 
 
 class TypingIndicator extends Component {
@@ -22,7 +23,15 @@ class TypingIndicator extends Component {
         if (memberTyping !== this.props.upi) {
             for (let i = 0; i < this.props.memberArray.length; i++) {
                 if (this.props.memberArray[i].upi === memberTyping) {
-                    return this.props.prevMessage.author === memberTyping ? <View style={styles.notMeBubble}>< Text style={styles.notMeMessageText}>TYPING</Text></View> : <View><Text style={styles.notMeAuthor}>{this.props.memberArray[i].firstName}</Text><View style={styles.notMeBubble}>< Text selectable style={styles.notMeMessageText}>TYPING</Text></View></View>
+                    return this.props.prevMessage.author === memberTyping ? <View style={styles.notMeBubble}><AnimatedEllipsis numberOfDots={3}
+                        minOpacity={0.4}
+                        animationDelay={200}
+                        style={styles.dots}
+                    /></View> : <View><Text style={styles.notMeAuthor}>{this.props.memberArray[i].firstName}</Text><View style={styles.notMeBubble}><AnimatedEllipsis numberOfDots={3}
+                        minOpacity={0.4}
+                        animationDelay={200}
+                        style={styles.dots}
+                    /></View></View>
                 }
             }
         }
@@ -46,10 +55,19 @@ const styles = StyleSheet.create({
         padding: 10,
         borderRadius: 20,
         marginBottom: 2.5,
-        marginLeft: 25
+        marginLeft: 25,
+        justifyContent: 'center',
+        height: 41.7,
+        width: 85
     },
-    notMeMessageText: {
-        fontSize: 18
+    dots: {
+        color: '#94939b',
+        fontSize: 100,
+        left: -10,
+        letterSpacing: -12.5,
+        textAlign: 'center',
+        top: -35,
+        height: 110,
     },
     notMeAuthor: {
         marginLeft: 35,

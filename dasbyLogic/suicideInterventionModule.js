eab7ffe72eab0df33f9f1809c51662281b714f1d
Seth Feder <sethfeder2017@u.northwestern.edu>
2019-01-22 22:08:39 -0600

    added suicide intervention module

---
+++ dasbyLogic/suicideInterventionModule.js
@@ -0,0 +1,96 @@
+'use strict'
+
+/************************************************
+ Imports 
+************************************************/
+const DasbyActions = require('./dasbyActions');
+const dbUsers = require('../models/Users');
+const twilio = require('../routes/services/twilio');
+
+// Item map = { id : [answer thresholds] }
+const itemMap = {
+    251: [4, 5],
+    379: [2],
+    380: [3, 4, 5],
+    381: [1, 2, 3],
+    382: [3, 4, 5],
+    383: [3, 4],
+    384: [3, 4],
+    385: [1],
+    386: [3, 4, 5],
+    387: [3, 4],
+    388: [2],
+    389: [2, 3, 4, 5]
+}
+
+/************************************************
+ Functions (Public)
+************************************************/
+
+// After a question is answered, check if it is a suicide question - and if so, compare the thresholds for them. 
+//If it was answered at or above the threshold for the particular question, trigger the suicide prevention protocol
+suicideCheck = (currentQuestion, choice, userUpi) => {
+
+    console.log("Running Suicide Trigger Protocol: ");
+
+    // Step 4: compare the answer given with the treshold value. If the treshold is met, 
+    // start the sucide protocol
+        let shouldTrigger = false;
+        const questionId = currentQuestion.questionID;
+        const thresholds = itemMap[questionId];
+        if (questionId in itemMap) {
+            if (thresholds.indexOf(choice) > -1) {
+                // Start protocol
+                takeAction(userUpi, "warn");
+                // suicidePreventionProtocol(userUpi);
+            }
+        }
+}
+
+// NEED TO REFACTOR THIS TO PROPERLY PROMPT DASBY SCRIPT
+// ALSO, ONLY WANT THIS TO BE TRIGGERED ONCE PER SURVEY
+// var suicidePreventionProtocol = function (sender) {
+//     twilio.findChannel(chatClient, this.state.userInfo.upi)
+//         .then(channel => {
+//             DasbyActions.read(channel.sid, "Suicide Protocol", 0, 0)
+//         })
+// }
+
+takeAction = (userUpi, action) => {
+    switch (action) {
+        case "warn":
+            const body = "has been thinking about suicide lately. \nNo immediate actions necessary.";
+            contactPI(userUpi, body);
+            break;
+        case "call_now":
+            const body = "needs your assistance. \n**PLEASE CALL IMMEDIATELY**";
+            contactPI(userUpi, body);
+            break;
+        case "follow_up":
+            const body = "needs you to follow up tomorrow.";
+            contactPI(userUpi, body);
+            break;
+    }
+
+}
+
+/************************************************
+ Functions (Private)
+************************************************/
+ contactPI = (userUpi, body) => {
+    console.log("CONTACTING PI");
+    dbUsers.findOne({ upi: userUpi })
+        .then(user => {
+            const message = `${user.first_name} ${user.last_name} ${body}`
+            twilio.sendSMS(message)
+        })
+}
+
+/************************************************
+ Modules 
+************************************************/
+module.exports = {
+    suicideWatch: suicideWatch,
+    suicideTrigger: suicideTrigger,
+    takeAction: takeAction
+}
